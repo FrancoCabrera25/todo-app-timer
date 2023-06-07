da878c94555ad762a40fdf43355ef2a9cd64a16a
Franco Cabrera <cabrera.franco@outlook.com>
2023-06-06 22:23:05 -0300

    add redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: "task",
-    pathMatch: "full",
-    loadChildren: () => import("./pages/task/task.module").then(m => m.TaskModule)
-  }
+    {
+        path: 'task',
+        pathMatch: 'full',
+        loadChildren: () =>
+            import('./pages/task/task.module').then((m) => m.TaskModule),
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/task',
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
